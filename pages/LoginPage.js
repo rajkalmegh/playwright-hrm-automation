1d8f0c7f268962625aa64b3c21f496ba87f91c74
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-01-21 18:37:19 +0530

    Update LoginPage.js

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,1 +1,20 @@
+export class LoginPage {
+
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = 'input[name="username"]';
+    this.passwordInput = 'input[name="password"]';
+    this.loginButton = 'button[type="submit"]';
+  }
+
+  async open() {
+    await this.page.goto('https://opensource-demo.orangehrmlive.com');
+  }
+
+  async login(username, password) {
+    await this.page.fill(this.usernameInput, username);
+    await this.page.fill(this.passwordInput, password);
+    await this.page.click(this.loginButton);
+  }
+}
 

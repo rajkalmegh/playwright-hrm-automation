8038a27a1bcd3e81ae83a37c1061069d5a4747ac
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-01-21 18:36:51 +0530

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,1 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('Valid login should redirect to dashboard', async ({ page }) => {
+
+  // 1. Open application
+  await page.goto('https://opensource-demo.orangehrmlive.com');
+
+  // 2. Enter username
+  await page.fill('input[name="username"]', 'Admin');
+
+  // 3. Enter password
+  await page.fill('input[name="password"]', 'admin123');
+
+  // 4. Click login
+  await page.click('button[type="submit"]');
+
+  // 5. Assertion
+  await expect(page).toHaveURL(/dashboard/);
+});
 
